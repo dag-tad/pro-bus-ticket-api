a6dc7efa12ce149e246b65cb6b6df20bfd353b02
dagTad <dagmawi.tadesse@eaglelionsystems.com>
2026-06-17 19:42:05 +0300

    update cookie sameSite property value

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -86,7 +86,7 @@ export class AuthController {
       response.cookie('refresh_token', result.refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax',
+        sameSite: process.env.NODE_ENV === 'development' ? 'lax' : 'none',
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         path: '/',
       });
