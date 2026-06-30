0d3ffdf02912b4bcb8887cec88b27deb00cea5b1
dagTad <dagmawi.tadesse@eaglelionsystems.com>
2026-06-30 21:23:39 +0300

    add listing user feature

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,58 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseUUIDPipe, Post, Query, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { ApiOperation } from '@nestjs/swagger';
+import { RequireAccess } from 'src/decorators/access.decorator';
+import { CurrentUser } from 'src/decorators/current-user.decorator';
+import { PaginationDto } from 'src/dto/pagination.dto';
+import { User } from 'src/entity/user.entity';
+import { REALM } from 'src/enums/realm.enum';
+import { ROLE } from 'src/enums/role.enum';
+import { NormalizeQueryPipe } from 'src/pipes/normalize-query.pipe';
+import { AccessTokenJWTGuard } from 'src/auth/guard/access-token-jwt.guard';
+import { AccessGuard } from 'src/auth/guard/access.guard';
 
 @Controller('user')
+@UseGuards(AccessTokenJWTGuard, AccessGuard)
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @ApiOperation({ summary: 'fetch users' })
+  @RequireAccess(
+    [REALM.SYSTEM, REALM.TRANSPORT_COMPANY],
+    [ROLE.SUPER_ADMIN, ROLE.COMPANY_ADMIN],
+  )
+  @Get('paginate')
+  async findAllBusses(
+    @CurrentUser() user: User,
+    @Query(new NormalizeQueryPipe()) options: PaginationDto,
+  ) {
+    const companyId = user.companyId ? user.companyId : options.companyId
+    return await this.userService.findAll({options, companyId});
+  }
+
+  @ApiOperation({ summary: 'count users' })
+  @RequireAccess(
+    [REALM.SYSTEM, REALM.TRANSPORT_COMPANY],
+    [ROLE.SUPER_ADMIN, ROLE.COMPANY_ADMIN],
+  )
+  @Get('count')
+  async countUsers(
+    @CurrentUser() user: User,
+    @Query(new NormalizeQueryPipe()) options: PaginationDto,
+  ) {
+    const companyId = user.companyId ? user.companyId : options.companyId
+    return await this.userService.countUsers(companyId);
+  }
+
+  @ApiOperation({ summary: 'count users' })
+  @RequireAccess(
+    [REALM.SYSTEM, REALM.TRANSPORT_COMPANY],
+    [ROLE.SUPER_ADMIN, ROLE.COMPANY_ADMIN],
+  )
+  @Get(':id')
+  async getUserDetail(
+     @Param('id', ParseUUIDPipe) id: string,
+  ) {
+    return await this.userService.getUserDetail(id);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -9,12 +9,12 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDTO } from '../dto/create-user.dto';
 import { User } from '../entity/user.entity';
 import { Repository } from 'typeorm';
+import { PaginationDto } from 'src/dto/pagination.dto';
+import { PaginatedResponse } from 'src/interfaces/paginatedResponse.interface';
 
 @Injectable()
 export class UserService {
-  constructor(
-    @InjectRepository(User) private repo: Repository<User>,
-  ) {}
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   async findOneByPhone(phone: string): Promise<User> {
     const user = await this.repo.findOneBy({
@@ -26,5 +26,108 @@ export class UserService {
     }
 
     return user;
+  }
+
+  async findAll(_data: {
+    options?: PaginationDto;
+    companyId?: string;
+  }): Promise<PaginatedResponse<User>> {
+    const { options, companyId } = _data;
+    const page = options?.page || 1;
+    const limit = options?.limit || 10;
+    const search = options?.search;
+    const sortBy = options?.sortBy;
+    const sortOrder = options?.sortOrder;
+
+    const skip = (page - 1) * limit;
+
+    const queryBuilder = this.repo.createQueryBuilder('users');
+
+    if (search) {
+      queryBuilder.where(
+        `users.firstName ILIKE :search OR users.lastName ILIKE :search`,
+        { search: `%${search}%` },
+      );
+    }
+
+    if (companyId) {
+      queryBuilder.andWhere('users.companyId = :companyId', {
+        companyId,
+      });
+    }
+
+    queryBuilder.leftJoinAndSelect('users.createdBy', 'createdBy');
+
+    const [data, totalItems] = await queryBuilder
+      .orderBy(`users.${sortBy}`, sortOrder)
+      .skip(skip)
+      .take(limit)
+      .getManyAndCount();
+
+    const totalPages = Math.ceil(totalItems / limit!);
+    const hasNextPage = page! < totalPages;
+    const hasPreviousPage = page! > 1;
+
+    return {
+      data,
+      meta: {
+        limit: limit!,
+        totalItems,
+        totalPages,
+        hasNextPage,
+        hasPreviousPage,
+        page: page!,
+      },
+    };
+  }
+
+  async countUsers(companyId?: string): Promise<number> {
+    const totalUsers = await this.repo.count({
+      where: {
+        companyId,
+      },
+    });
+
+    return totalUsers;
+  }
+
+  async getUserDetail(
+    id: string,
+  ): Promise<
+    Partial<User> & {
+      companyId?: string;
+      companyName?: string;
+      companyPhone?: string;
+      companyRegion?: string;
+      companyCity?: string;
+    }
+  > {
+    const user = await this.repo
+      .createQueryBuilder('user')
+      .leftJoinAndSelect('user.company', 'company')
+      .where('user.id = :id', { id })
+      .getOne();
+
+    if (!user) {
+      throw new NotFoundException(`User with id = ${id} not found.`);
+    }
+    
+    return {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      realm: user.realm,
+      role: user.role,
+      email: user.email,
+      phone: user.phone,
+      enable2FA: user.enable2FA,
+      createdAt: user.createdAt,
+      enabled: user.enabled,
+      companyId: user.companyId,
+      companyName: user.company?.name,
+      companyPhone: user.company?.phoneNumber,
+      companyCity: user.company?.city,
+      companyRegion: user.company?.region,
+    };
   }
 }
