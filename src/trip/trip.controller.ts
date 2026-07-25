a143c64f07d6c628e2529b06fa592cb5438e5608
dagTad <dagmawi.tadesse@eaglelionsystems.com>
2026-07-26 02:41:33 +0300

    add trip-stat feature

--- src/trip/trip.controller.ts
+++ src/trip/trip.controller.ts
@@ -1,5 +1,11 @@
 import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AccessTokenJWTGuard } from 'src/auth/guard/access-token-jwt.guard';
 import { AccessGuard } from 'src/auth/guard/access.guard';
 import { TripService } from './trip.service';
@@ -20,10 +26,10 @@ export class TripController {
   constructor(private service: TripService) {}
 
   @ApiOperation({ summary: 'fetch busses' })
-    @RequireAccess(
-      [REALM.SYSTEM, REALM.TRANSPORT_COMPANY],
-      [ROLE.SUPER_ADMIN, ROLE.COMPANY_ADMIN],
-    )
+  @RequireAccess(
+    [REALM.SYSTEM, REALM.TRANSPORT_COMPANY],
+    [ROLE.SUPER_ADMIN, ROLE.COMPANY_ADMIN],
+  )
   @Get('busses')
   async getBusses(
     @CurrentUser() user: User,
@@ -32,25 +38,39 @@ export class TripController {
     return await this.service.findAll(options, user.companyId);
   }
 
+  @ApiOperation({ summary: 'fetch busses' })
+  @RequireAccess(
+    [REALM.SYSTEM, REALM.TRANSPORT_COMPANY],
+    [ROLE.SUPER_ADMIN, ROLE.COMPANY_ADMIN],
+  )
+  @Get('stats')
+  async getStats(@CurrentUser() user: User) {
+    const _user = user as unknown as any;
+    const companyId = user.companyId ? user.companyId : user.companyId;
+
+    return await this.service.getStats(companyId!);
+  }
+
   @Post('create')
-    @RequireAccess(
-      [REALM.SYSTEM, REALM.TRANSPORT_COMPANY],
-      [ROLE.SUPER_ADMIN, ROLE.COMPANY_ADMIN],
-    )
-    @ApiOperation({ summary: 'Create trip for a company' })
-    @ApiBody({
-      type: CreateTripDTO,
-      description: 'Create a new trip for a company',
-    })
-    @ApiResponse({ status: 201, description: 'Trip created successfully' })
-    @ApiResponse({ status: 400, description: 'Bad request' })
-    async createTrip(
-      @CurrentUser() user: User,
-      @Body() data: CreateTripDTO,
-    ) {
-      const _user = user as unknown as any;
-      const companyId = user.companyId ? user.companyId : user.companyId;
-      
-      return await this.service.create({ userId: _user.userId, trip: data, companyId: companyId! });
-    }
+  @RequireAccess(
+    [REALM.SYSTEM, REALM.TRANSPORT_COMPANY],
+    [ROLE.SUPER_ADMIN, ROLE.COMPANY_ADMIN],
+  )
+  @ApiOperation({ summary: 'Create trip for a company' })
+  @ApiBody({
+    type: CreateTripDTO,
+    description: 'Create a new trip for a company',
+  })
+  @ApiResponse({ status: 201, description: 'Trip created successfully' })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  async createTrip(@CurrentUser() user: User, @Body() data: CreateTripDTO) {
+    const _user = user as unknown as any;
+    const companyId = user.companyId ? user.companyId : user.companyId;
+
+    return await this.service.create({
+      userId: _user.userId,
+      trip: data,
+      companyId: companyId!,
+    });
+  }
 }
